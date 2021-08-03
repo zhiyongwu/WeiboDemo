9892bb17079ca5d8a10ce9087839a3e0fffb2f6a
wuzhiyong <zhiyongwu@live.cn>
2021-08-03 20:14:50 +0800

    add function

--- WeiboDemo/ContentView.swift
+++ WeiboDemo/ContentView.swift
@@ -9,8 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        f(name: "hello there")
     }
 }
 
@@ -18,4 +17,9 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+
+func f(name:String) -> some View {
+    return Text(name.uppercased()).padding()
 }
