e11540081d30ea72ed3ade4dad02c9619355104b
wuzhiyong <zhiyongwu@live.cn>
2021-08-03 20:03:47 +0800

    Initial Commit

---
+++ WeiboDemo/WeiboDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeiboDemoApp.swift
+//  WeiboDemo
+//
+//  Created by wuzhiyong on 2021/8/3.
+//
+
+import SwiftUI
+
+@main
+struct WeiboDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
