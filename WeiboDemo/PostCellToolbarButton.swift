b29179652e816f08041fd16e30c53e0688744f91
wuzhiyong <zhiyongwu@live.cn>
2021-08-11 20:50:54 +0800

    add post list

---
+++ WeiboDemo/PostCellToolbarButton.swift
@@ -0,0 +1,20 @@
+//
+//  PostCellToolbarButton.swift
+//  WeiboDemo
+//
+//  Created by wuzhiyong on 2021/8/11.
+//
+
+import SwiftUI
+
+struct PostCellToolbarButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PostCellToolbarButton_Previews: PreviewProvider {
+    static var previews: some View {
+        PostCellToolbarButton()
+    }
+}
---
+++ WeiboDemo/PostListView.swift
@@ -0,0 +1,20 @@
+//
+//  PostListView.swift
+//  WeiboDemo
+//
+//  Created by wuzhiyong on 2021/8/11.
+//
+
+import SwiftUI
+
+struct PostListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PostListView_Previews: PreviewProvider {
+    static var previews: some View {
+        PostListView()
+    }
+}
